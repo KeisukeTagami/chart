cdc67aa55123fd4b6b4ae9e92338191ca166c659
Keisuke Tagami <tagami@xcomass.com>
2016-08-20 02:52:42 +0900

    xAxis drag field

--- Main.elm
+++ Main.elm
@@ -28,7 +28,9 @@ type alias Model =
     , xMin : Float
     , yMax : Float
     , yMin : Float
+    , xTickCount : Int
     , yTickCount : Int
+    , dragStartAt : Maybe (Int, Int)
     }
 
 init : ( Model, Cmd Msg )
@@ -39,13 +41,15 @@ emptyModel =
   { field = ""
   , width = 800
   , height = 400
-  , margin = (80,20)
+  , margin = (80,30)
   , tickSize = 6
   , xMax = 600
-  , xMin = 0
+  , xMin = -100
   , yMax = 100
-  , yMin = 0
+  , yMin = -10
+  , xTickCount = 5
   , yTickCount = 5
+  , dragStartAt = Nothing
   }
 
 translate (x, y) = "translate(" ++  toString x ++ "," ++ toString y ++ ")"
@@ -69,18 +73,18 @@ toD x y ps =
       g (a,b) = toString (x a) ++ "," ++ toString (y b)
   in d <| String.join "," <| List.map f ps
 
-toX model a = ( a - model.xMin ) / ( model.xMax - model.xMin) * ( toFloat ( model.width - fst model.margin) )
-toY model a = ( 1.0 - ( a - model.yMin ) / ( model.yMax - model.yMin) ) * ( toFloat ( model.height - snd model.margin) )
+toX model a = ( a - model.xMin ) / ( model.xMax - model.xMin) * ( toFloat ( model.width - ( fst model.margin ) * 2 ) )
+toY model a = ( 1.0 - ( a - model.yMin ) / ( model.yMax - model.yMin) ) * ( toFloat ( model.height - ( snd model.margin ) * 2 ) )
 
 yAxis model = ( Svg.path [ toD (toX model) (toY model) [ M (0 , model.yMax)
-                                                       , V 0
+                                                       , V model.yMin
                                                        ]
                          , Attr.style "stroke: rgb(31, 119, 180); fill: none;"
                          ] []
               ) :: ( List.map (\ i ->
-                                   let j = model.yMax * toFloat i / toFloat model.yTickCount
+                                   let j = (model.yMax - model.yMin) * toFloat i / toFloat model.yTickCount + model.yMin
                                    in g [ transform <| translate (0, toY model j)
-                                     ]
+                                        ]
                                    [ line [ stroke "#000"
                                           , x1 <| toString <| toX model model.xMax
                                           , x2 <| toString <| -1 * model.tickSize
@@ -98,12 +102,32 @@ yAxis model = ( Svg.path [ toD (toX model) (toY model) [ M (0 , model.yMax)
                                    ]
                                 ) [0..model.yTickCount]
                    )
-xAxis model = [ Svg.path [ toD (toX model) (toY model) [ M (model.xMin, 0)
+xAxis model = ( Svg.path [ toD (toX model) (toY model) [ M (model.xMin, 0)
                                                        , H model.xMax
                                                        ]
                          , Attr.style "stroke: rgb(31, 119, 180); fill: none;"
                          ] []
-              ]
+              ) :: ( List.map (\ i ->
+                                   let j = (model.xMax - model.xMin) * toFloat i / toFloat model.xTickCount + model.xMin
+                                   in g [ transform <| translate (toX model j, toY model model.yMin)
+                                        ]
+                                   [ line [ stroke "#000"
+                                          , x1 "0"
+                                          , x2 "0"
+                                          , y1 <| toString <| -1 * ( toY model ( model.yMin + model.yMin) )
+                                          , y2 <| toString <| model.tickSize
+                                          ]
+                                       []
+                                   , let size = Utils.size <| text' [] [t]
+                                         t = text <| Formatting.print (Formatting.roundTo 2) j
+                                     in text' [ x "0"
+                                              , y "0"
+                                              , dx ("-" ++ ( toString <| (toFloat ( fst size) / 2) ) )
+                                              , dy ( toString <| (toFloat <| snd size) + model.tickSize )
+                                              ] [ t ]
+                                   ]
+                                ) [0..model.xTickCount]
+                   )
 
 
 view model =
@@ -117,6 +141,27 @@ view model =
                 "wheel"
                 { stopPropagation = True, preventDefault = True }
                 ( Decode.object1 Wheel ( "deltaY" := Decode.float) )
+          , onWithOptions
+                "mousedown"
+                { stopPropagation = True, preventDefault = True }
+                ( Decode.object2 MouseDown
+                      ( "clientX" := Decode.int)
+                      ( "clientY" := Decode.int)
+                )
+          , onWithOptions
+                "mouseup"
+                { stopPropagation = True, preventDefault = True }
+                ( Decode.object2 MouseUp
+                      ( "clientX" := Decode.int)
+                      ( "clientY" := Decode.int)
+                )
+          , onWithOptions
+                "mousemove"
+                { stopPropagation = True, preventDefault = True }
+                ( Decode.object2 MouseMove
+                      ( "clientX" := Decode.int)
+                      ( "clientY" := Decode.int)
+                )
           ] [
            g [ transform <| translate model.margin
              ]
@@ -133,7 +178,7 @@ view model =
           ]
     ]
 
-type Msg = Increment | Decrement | Wheel Float
+type Msg = Increment | Decrement | Wheel Float | MouseDown Int Int | MouseUp Int Int | MouseMove Int Int
 
 update msg model =
   case msg of
@@ -144,5 +189,16 @@ update msg model =
                                         else model.yTickCount - 1
                  , field = Utils.log <| model.field } ! []
     Wheel dy -> { model | yMax = model.yMax * ( 1000 + dy ) / 1000
+                , yMin = model.yMin * ( 1000 + dy ) / 1000
                 , xMax = model.xMax * ( 1000 + dy ) / 1000
+                , xMin = model.xMin * ( 1000 + dy ) / 1000
                 } ! []
+    MouseDown x y -> { model | field = toString x ++ " down " ++ toString y
+                     , dragStartAt = Just (x, y)
+                     } ! []
+    MouseUp x y -> { model | field = toString x ++ " up " ++ toString y
+                   , dragStartAt = Nothing } ! []
+    -- MouseMove x y -> case model.dragStartAt of
+    --                      Just _ -> { model | field = toString x ++ " move " ++ toString y} ! []
+    --                      Nothing -> model ! []
+    MouseMove x y -> { model | field = toString x ++ " move " ++ toString y} ! []
